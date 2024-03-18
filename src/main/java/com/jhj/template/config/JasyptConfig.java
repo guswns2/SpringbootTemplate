79737ff39ed56ef0ec14d115f50bdf146db8a60d
guswns2 <wkdguswns6@gmail.com>
2024-03-18 21:09:17 +0900

    jasypt 암복호화 키 하드코딩

--- src/main/java/com/jhj/template/config/JasyptConfig.java
+++ src/main/java/com/jhj/template/config/JasyptConfig.java
@@ -3,7 +3,6 @@ package com.jhj.template.config;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,16 +13,13 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 public class JasyptConfig {
   // application.properties 암/복호화
   // jasypt 라이브러리 설정
-  @Value("${jasypt.encryptor.password}")
-  private String encryptKey;
 
   @Bean(name = "jasyptStringEncryptor")
   public StringEncryptor stringEncryptor() {
     PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
     SimpleStringPBEConfig config = new SimpleStringPBEConfig();
 
-    config.setPassword(encryptKey); // 암호화할 때 사용하는 키
-    // config.setAlgorithm("PBEWithMD5AndTripleDES"); // 암호화 알고리즘
+    config.setPassword("b7878370-f5a6-4f9c-9aca-741253f71455"); // 암호화할 때 사용하는 키
     config.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
     config.setKeyObtentionIterations("1000"); // 반복할 해싱 회수
     config.setPoolSize("1"); // 인스턴스 pool
